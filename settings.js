eef43fe23e4705df2741e9d70c017c7dc3049ed6
HilariWhatsapp <hilariwhatsapp@gmail.com>
2025-03-29 17:20:29 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
   packname: 'Knight',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "Creator2 bot",
+  botOwner: 'creator2', // Your name
+  ownerNumber: '255710474559', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
